277b903fd89aefd76b41bad5acc09cd246ead6c3
David LeGare <excaliburhissheath@gmail.com>
2017-06-17 09:21:41 -0500

    Remove Phaser because I said so

--- www/host.js
+++ www/host.js
@@ -1,44 +1,15 @@
 'use strict';
 
-// Initialize the VueJS app. This is used for debug rendering.
-let debug = new Vue({
+// Initialize the VueJS app. This is used for app rendering.
+let app = new Vue({
     el: '#vue-root',
     data: {
-        isDebugEnabled: true,
         players: [],
-    },
-});
-
-// Initialize the Phaser game. Phaser provides most of the functionality that
-// we use to build the display.
-let game = new Phaser.Game({
-    width: 800,
-    height: 600,
-    parent: 'phaser-root',
-    state: {
-        preload: preload,
-        create: create,
+        playerMap: {},
     },
 });
 
 let players = [];
-
-function preload() {
-    game.load.image('hippo', 'assets/hippo.jpg');
-    game.load.image('background', 'assets/backdrop.jpg');
-}
-
-function create() {
-    // Create a sprite for the backdrop image and have it fill the canvas.
-    let background = game.add.sprite(0, 0, 'background');
-    background.width = 800;
-    background.height = 600;
-
-    // Create a sprite for the hippo head and make is relatively small.
-    let sprite = game.add.sprite(80, 0, 'hippo');
-    sprite.width = 100;
-    sprite.height = 100;
-}
 
 // Setup a websocket to listen for updates from the server.
 let socket = new WebSocket('ws://' + window.location.hostname + ':6769');
@@ -48,27 +19,24 @@ socket.onmessage = (event) => {
 
     if (payload['PlayerRegistered']) {
         let player = payload['PlayerRegistered'];
-        players.push(player);
+        app.players.push(player);
+        app.playerMap[player.id] = player;
     } else if (payload['PlayerScore']) {
         let info = payload['PlayerScore'];
 
-        // Find the existing score object in `debug.scores`. If the player is already in the list,
-        // then update their score, otherwise add them to the list.
-        // TODO: We probaly shouldn't try to track score for players that haven't been registered,
-        // since we wouldn't know there username.
-        let existing_score = players.find(player => player.id === info.id);
-        assert(existing_score != null, 'Got a score for an unregistered player: ' + info.player);
-        existing_score.score = info.score;
+        let player = app.playerMap[info.id];
+        assert(player != null, 'Unable to find player for ID: ' + info.id);
+
+        player.score = info.score;
     }
 };
 
 // When we first boot up we need to get the current list of players.
 get('/api/players', response => {
-    players = response['players'];
+    app.players = response['players'];
 
-    // Set the `debug.players` to be `players`, that way any changes to
-    // `players` will automatically be reflected in the debug information.
-    debug.players = players;
-
-    // TODO: Create a hippo for each player.
+    // Add players to the player map, so we can find them by ID.
+    for (let player of app.players) {
+        app.playerMap[player.id] = player;
+    }
 });
