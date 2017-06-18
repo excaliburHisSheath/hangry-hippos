baab2e87a7a115e415584bce690829431681dee8
David LeGare <david.legare@scientificgames.com>
2017-06-18 15:49:51 -0500

    Add some comments and stuff

--- www/host.js
+++ www/host.js
@@ -50,17 +50,19 @@ socket.onmessage = (event) => {
     let payload = JSON.parse(event.data);
 
     if (payload['PlayerRegistered']) {
-        let player = payload['PlayerRegistered'];
-        registerPlayer(player);
+        registerPlayer(payload['PlayerRegistered']);
     } else if (payload['PlayerScore']) {
         let info = payload['PlayerScore'];
 
+        // Find the hippo/player for the player that scored.
         let hippo = app.hippoMap[info.id];
         assert(hippo != null, 'Unable to find hippo for ID: ' + info.id);
 
+        // Updated the local score for the player.
         hippo.player.score = info.score;
 
-        // Animate the hippo head to match the score increase.
+        // Animate the hippo head to match the score increase. The direction of the chomp animation
+        // depends on the side of the screen that the hippo is on.
         let element = document.getElementById(hippo.player.id);
         switch (hippo.side) {
             case TOP_SIDE: {
@@ -115,15 +117,22 @@ get('/api/players', response => {
     }
 });
 
+/**
+ * Creates a hippo for the new player and adds it to one side of the screen.
+ */
 function registerPlayer(player) {
+    // Create a hippo object for the player.
     let hippo = {
         player: player,
         side: currentSide,
     };
 
+    // Add the hippo to the hippo map.
     assert(app.hippoMap[player.id] == null, 'Hippo already exists for ID: ' + player.id);
     app.hippoMap[player.id] = hippo;
-    sides[currentSide].push(hippo);
 
+    // Add the hippo to one of the sides of the screen. We cycle the sides clockwise to make sure
+    // the hippos are distributed evenly.
+    sides[currentSide].push(hippo);
     currentSide = (currentSide + 1) % 4;
 }
